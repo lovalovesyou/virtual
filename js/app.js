80584b6203408d95fcb4162235690629cdf549da
lovalovesyou <lovalovesyou8@gmail.com>
2026-04-23 16:33:22 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,40 @@
-function logout() {
-  localStorage.removeItem("user");
-  window.location.href = "index.html";
-}
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { 
+  getAuth, 
+  onAuthStateChanged, 
+  signOut 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-function goToBuilder() {
-  window.location.href = "builder.html";
-}
+// 🔴 MISMO CONFIG AQUÍ
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  appId: "TU_APP_ID"
+};
 
-function goToCart() {
-  window.location.href = "cart.html";
-}
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 // PROTEGER HOME
-(function () {
-  const user = localStorage.getItem("user");
-
+onAuthStateChanged(auth, (user) => {
   if (!user) {
     window.location.href = "index.html";
   }
-})();
+});
+
+// LOGOUT
+window.logout = function () {
+  signOut(auth).then(() => {
+    window.location.href = "index.html";
+  });
+};
+
+// NAV
+window.goToBuilder = function () {
+  window.location.href = "builder.html";
+};
+
+window.goToCart = function () {
+  window.location.href = "cart.html";
+};
